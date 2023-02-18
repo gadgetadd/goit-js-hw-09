e1bfecb5e6d9e65e3ef53c1dcc3847d2fa6f1796
Maksym Ostapchuk <gadget.add@gmail.com>
2023-02-18 14:45:26 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let colorChangeInterval;
 
+const getRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const onStart = e => {
+  colorChangeInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+};
+const onStop = e => {
+  clearInterval(colorChangeInterval);
+  startBtn.disabled = false;
+};
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
